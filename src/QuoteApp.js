feab8409dbcc0923e3758df10a37654c2b81f76b
sakanana0210 <135511792+sakanana0210@users.noreply.github.com>
2023-07-28 00:09:16 +0800

    Update QuoteApp.js

--- src/QuoteApp.js
+++ src/QuoteApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { connect } from 'react-redux';
 import addQuote from './actions/quoteActions';
 import './index.css';
